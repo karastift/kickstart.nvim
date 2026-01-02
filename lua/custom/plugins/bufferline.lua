4c1ba679f07cb02bf6ea71c99275fe24edfd3c8b
karastift <69339287+karastift@users.noreply.github.com>
2026-01-02 14:35:30 +0300

    auto-session

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,43 +3,41 @@ return {
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      vim.keymap.set('n', '<Tab>', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer' })
-      vim.keymap.set('n', '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Prev buffer' })
+    event = 'VeryLazy',
 
-      require('bufferline').setup {
-        options = {
+    -- keymaps can stay here
+    keys = {
+      { '<Tab>', '<cmd>BufferLineCycleNext<CR>', desc = 'Next buffer' },
+      { '<S-Tab>', '<cmd>BufferLineCyclePrev<CR>', desc = 'Prev buffer' },
+    },
 
-          -- Hide bufferline when no real files are open
-          always_show_bufferline = false,
+    opts = {
+      options = {
+        -- Hide bufferline when no real files are open
+        always_show_bufferline = false,
 
-          -- Don’t show special buffers
-          custom_filter = function(buf)
-            -- hide unnamed buffers
-            if vim.fn.bufname(buf) == '' then
-              return false
-            end
+        -- Don’t show special buffers
+        custom_filter = function(buf)
+          if vim.fn.bufname(buf) == '' then
+            return false
+          end
+          if vim.bo[buf].buftype ~= '' then
+            return false
+          end
+          return true
+        end,
 
-            -- hide non-file buffers (help, neo-tree, ...)
-            if vim.bo[buf].buftype ~= '' then
-              return false
-            end
+        diagnostics = 'nvim_lsp',
 
-            return true
-          end,
-
-          diagnostics = 'nvim_lsp',
-
-          offsets = {
-            {
-              filetype = 'neo-tree',
-              text = '',
-              highlight = 'Directory',
-              separator = true,
-            },
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = '',
+            highlight = 'Directory',
+            separator = true,
           },
         },
-      }
-    end,
+      },
+    },
   },
 }
